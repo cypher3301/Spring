c559c5d36e12965c1f7480cfad43b6daf8395442
Admin <cypher.is.3301@gmail.com>
2021-02-04 09:22:43 +0200

    Dependency injection with setter and xml configuration

--- src/main/java/ua/mytestprojects/springapp/music/MusicPlayer.java
+++ src/main/java/ua/mytestprojects/springapp/music/MusicPlayer.java
@@ -3,6 +3,10 @@ package ua.mytestprojects.springapp.music;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
+
     //IoC
     public MusicPlayer(Music music) {
         this.music = music;
@@ -17,5 +21,21 @@ public class MusicPlayer {
 
     public void setMusic(Music music) {
         this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
