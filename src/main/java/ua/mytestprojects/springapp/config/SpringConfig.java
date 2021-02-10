549f2c35272e879917791456368c7d0034854628
Admin <cypher.is.3301@gmail.com>
2021-02-10 22:54:43 +0200

    deleted jsp, xml configuration files. add settings to SpringConfig

--- src/main/java/ua/mytestprojects/springapp/config/SpringConfig.java
+++ src/main/java/ua/mytestprojects/springapp/config/SpringConfig.java
@@ -1,12 +1,47 @@
 package ua.mytestprojects.springapp.config;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
 
 @Configuration
 @ComponentScan("ua.mytestprojects.springapp")
 @EnableWebMvc
-public class SpringConfig {
+public class SpringConfig  implements WebMvcConfigurer {
+    private final ApplicationContext applicationContext;
 
+    public SpringConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/views");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine  = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(resolver);
+    }
 }
--- src/main/java/ua/mytestprojects/springapp/main/TestSpring.java
+++ src/main/java/ua/mytestprojects/springapp/main/TestSpring.java
@@ -5,11 +5,11 @@ import ua.mytestprojects.springapp.config.SpringConfig;
 import ua.mytestprojects.springapp.beans.*;
 
 public class TestSpring {
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        System.out.println(musicPlayer.playMusicGenre());
-        context.close();
-    }
+//    public static void main(String[] args) {
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+//
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        System.out.println(musicPlayer.playMusicGenre());
+//        context.close();
+//    }
 }
