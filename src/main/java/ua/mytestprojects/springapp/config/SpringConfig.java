94c87c6c168885f9ab8a7ba584a012750dae84a9
Admin <cypher.is.3301@gmail.com>
2021-02-17 10:23:13 +0200

    Thmeleaf: 2. SpringStandard Диалект. Java config: SpringConfig.java, Xml config: applicationContextMVC.xml

--- src/main/java/ua/mytestprojects/springapp/config/SpringConfig.java
+++ src/main/java/ua/mytestprojects/springapp/config/SpringConfig.java
@@ -15,7 +15,7 @@ import org.thymeleaf.templatemode.TemplateMode;
 @Configuration
 @EnableWebMvc
 @ComponentScan("ua.mytestprojects.springapp")
-public class SpringConfig  implements WebMvcConfigurer {
+public class SpringConfig implements WebMvcConfigurer {
     private final ApplicationContext applicationContext;
 
     public SpringConfig(ApplicationContext applicationContext) {
@@ -23,21 +23,37 @@ public class SpringConfig  implements WebMvcConfigurer {
     }
 
     @Bean
-    public SpringResourceTemplateResolver templateResolver(){
+    public SpringResourceTemplateResolver templateResolver() {
+        // SpringResourceTemplateResolver automatically integrates with Spring's own
+        // resource resolution infrastructure, which is highly recommended.
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setApplicationContext(applicationContext);
-//        templateResolver.setTemplateMode(TemplateMode.HTML);//режим шаблона
-//        templateResolver.setCacheTTLMs(36000000L);//время нахождения в кеше
+
         templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);//режим шаблона, HTML is the default value
+        templateResolver.setCacheTTLMs(36000000L);//время нахождения в кеше
+        templateResolver.setCheckExistence(false);
+        templateResolver.setOrder(1);
+        // Template cache is true by default. Set to false if you want
+        // templates to be automatically updated when modified.
+        templateResolver.setCacheable(true);
         return templateResolver;
     }
 
     @Bean
-    public SpringTemplateEngine templateEngine(){
-        SpringTemplateEngine templateEngine  = new SpringTemplateEngine();
+    public SpringTemplateEngine templateEngine() {
+        // SpringTemplateEngine automatically applies SpringStandardDialect and
+        // enables Spring's own MessageSource message resolution mechanisms.
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
+        // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
+        // speed up execution in most scenarios, but might be incompatible
+        // with specific cases when expressions in one template are reused
+        // across different data types, so this flag is "false" by default
+        // for safer backwards compatibility.
         templateEngine.setEnableSpringELCompiler(true);
+
         return templateEngine;
     }
 
